1371d583c1cf1297c904479d984f8f343ab03e6e
Emilo067 <gasanovemil467@gmail.com>
2024-05-11 10:27:31 +0300

    Create AddItemForm.tsx

---
+++ src/common/components/AddItemForm/AddItemForm.tsx
@@ -0,0 +1,57 @@
+import TextField from '@mui/material/TextField/TextField'
+import {ChangeEvent, KeyboardEvent, memo, useState} from 'react'
+import AddBoxIcon from '@mui/icons-material/AddBox'
+import IconButton from '@mui/material/IconButton'
+
+type PropsType = {
+    addItem: (title: string) => void
+}
+
+export const AddItemForm = memo(({addItem}: PropsType) => {
+
+    console.log("AddItemForm rerendered")
+
+    const [title, setTitle] = useState('')
+    const [error, setError] = useState<string | null>(null)
+
+    const addItemHandler = () => {
+        if (title.trim() !== '') {
+            addItem(title.trim())
+            setTitle('')
+        } else {
+            setError('Title is required')
+        }
+    }
+
+    const changeItemHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
+    }
+
+    const addItemOnKeyUpHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (error !== null) {
+            setError(null)
+        }
+        if (event.key === 'Enter') {
+            addItemHandler()
+        }
+    }
+
+    return (
+        <div>
+            <TextField
+                label="Enter a title"
+                variant={'outlined'}
+                className={error ? 'error' : ''}
+                value={title}
+                error={!!error}
+                helperText={error}
+                size={'small'}
+                onChange={changeItemHandler}
+                onKeyUp={addItemOnKeyUpHandler}
+            />
+            <IconButton onClick={addItemHandler} color={'primary'}>
+                <AddBoxIcon/>
+            </IconButton>
+        </div>
+    )
+})
