f7eef1abc08b48ff23791c0420d6b0f400a7003a
Emilo067 <gasanovemil467@gmail.com>
2024-06-10 18:31:32 +0300

    Sprint 5

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -1,39 +1,37 @@
-import type {Meta, StoryObj} from '@storybook/react';
-import {fn} from "@storybook/test";
-import {Task} from "../features/TodolistList/Todolist/Task/Task";
-import {useState} from "react";
-import {action} from "@storybook/addon-actions";
-import {TaskPriorities, TaskStatuses} from "../api/todolist-api";
+import type { Meta, StoryObj } from "@storybook/react";
+import { Task } from "features/TodolistList/ui/Todolist/TasksList/Task/Task";
+import { useState } from "react";
+import { TaskPriorities, TaskStatuses } from "common/enums/enums";
 
 // More on how to set up stories at:
 // https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof Task> = {
-    title: 'TODOLISTS/Task',
-    component: Task,
-    parameters: {
-        layout: ['centered']
+  title: "TODOLISTS/Task",
+  component: Task,
+  parameters: {
+    layout: ["centered"],
+  },
+  // This component will have an automatically generated Autodocs entry:
+  // https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes:
+  args: {
+    task: {
+      id: "taskId",
+      title: "JS",
+      status: TaskStatuses.Completed,
+      description: "test task",
+      completed: false,
+      priority: TaskPriorities.Low,
+      startDate: "",
+      deadline: "",
+      todoListId: "todolistId1",
+      order: 0,
+      addedDate: "",
     },
-    // This component will have an automatically generated Autodocs entry:
-    // https://storybook.js.org/docs/react/writing-docs/autodocs
-    tags: ['autodocs'],
-    // More on argTypes:
-    args: {
-        task: {
-            id: 'taskId', title: 'JS', status: TaskStatuses.Completed, description: 'test task',
-            completed: false,
-            priority: TaskPriorities.Low,
-            startDate: '',
-            deadline: '',
-            todoListId: 'todolistId1',
-            order: 0,
-            addedDate: ''
-        },
-        todolistId: 'totolistId',
-        changeTaskStatus: fn(),
-        changeTaskTitle: fn(),
-        removeTask: fn()
-    }
-    // https://storybook.js.org/docs/react/api/argtypes
+    todolistId: "totolistId",
+  },
+  // https://storybook.js.org/docs/react/api/argtypes
 };
 
 export default meta;
@@ -42,38 +40,40 @@ type Story = StoryObj<typeof Task>;
 // More on component templates:
 // https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 export const TaskIsDoneStory: Story = {
-    args: {}
-}
+  args: {},
+};
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 
 export const TaskIsNotDoneStory: Story = {
-    args: {
-        task: {
-            id: 'taskId', title: 'JS', status: TaskStatuses.New, description: 'test task',
-            completed: false,
-            priority: TaskPriorities.Low,
-            startDate: '',
-            deadline: '',
-            todoListId: 'todolistId1',
-            order: 0,
-            addedDate: ''
-        },
-    }
-}
+  args: {
+    task: {
+      id: "taskId",
+      title: "JS",
+      status: TaskStatuses.New,
+      description: "test task",
+      completed: false,
+      priority: TaskPriorities.Low,
+      startDate: "",
+      deadline: "",
+      todoListId: "todolistId1",
+      order: 0,
+      addedDate: "",
+    },
+  },
+};
 
 export const TaskToggleStory: Story = {
-    render: (args) => {
-        const [task, setTask] = useState(args.task)
+  render: (args) => {
+    const [task, setTask] = useState(args.task);
 
-        function changeTaskStatus() {
-            setTask({...task, status: task.status === TaskStatuses.New ? TaskStatuses.Completed : TaskStatuses.New})
-        }
-
-        function changeTaskTitle(todolistId: string, taskId: string, title: string) {
-            setTask({...task, title})
-        }
-
-        return <Task entityStatus={'idle'} task={task} todolistId={'todolistId'} changeTaskStatus={changeTaskStatus}
-                     changeTaskTitle={changeTaskTitle} removeTask={action('removed')}/>
+    function changeTaskStatus() {
+      setTask({ ...task, status: task.status === TaskStatuses.New ? TaskStatuses.Completed : TaskStatuses.New });
     }
-}
+
+    function changeTaskTitle(todolistId: string, taskId: string, title: string) {
+      setTask({ ...task, title });
+    }
+
+    return <Task entityStatus={"idle"} task={task} todolistId={"todolistId"} />;
+  },
+};
