3ee9b0f610ec4da75578c69a52d177c9010e7854
Emilo067 <gasanovemil467@gmail.com>
2024-06-10 18:13:58 +0300

    Sprint 5

--- src/common/components/AddItemForm/AddItemForm.tsx
+++ src/common/components/AddItemForm/AddItemForm.tsx
@@ -1,58 +1,63 @@
-import TextField from '@mui/material/TextField/TextField'
-import {ChangeEvent, KeyboardEvent, memo, useState} from 'react'
-import AddBoxIcon from '@mui/icons-material/AddBox'
-import IconButton from '@mui/material/IconButton'
+import TextField from "@mui/material/TextField/TextField";
+import { ChangeEvent, KeyboardEvent, memo, useState } from "react";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import IconButton from "@mui/material/IconButton";
+import { ResponseType } from "common/types/ResponseType";
 
 export type AddItemFormPropsType = {
-    addItem: (title: string) => void
-    disabled?: boolean
-}
+  addItem: (title: string) => Promise<any>;
+  disabled?: boolean;
+};
 
-export const AddItemForm = memo(({addItem, disabled}: AddItemFormPropsType) => {
+export const AddItemForm = memo(({ addItem, disabled = false }: AddItemFormPropsType) => {
+  console.log("AddItemForm rerendered");
 
-    console.log("AddItemForm rerendered")
+  const [title, setTitle] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
-    const [title, setTitle] = useState('')
-    const [error, setError] = useState<string | null>(null)
-
-    const addItemHandler = () => {
-        if (title.trim() !== '') {
-            addItem(title.trim())
-            setTitle('')
-        } else {
-            setError('Title is required')
-        }
+  const addItemHandler = () => {
+    if (title.trim() !== "") {
+      addItem(title.trim())
+        .then((res: ResponseType) => {
+          setTitle("");
+        })
+        .catch((rej: ResponseType) => {
+          setError(rej.messages[0]);
+        });
+    } else {
+      setError("Title is required");
     }
+  };
 
-    const changeItemHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        setTitle(event.currentTarget.value)
+  const changeItemHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.currentTarget.value);
+  };
+
+  const addItemOnKeyUpHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (error !== null) {
+      setError(null);
     }
-
-    const addItemOnKeyUpHandler = (event: KeyboardEvent<HTMLInputElement>) => {
-        if (error !== null) {
-            setError(null)
-        }
-        if (event.key === 'Enter') {
-            addItemHandler()
-        }
+    if (event.key === "Enter") {
+      addItemHandler();
     }
+  };
 
-    return (
-        <div>
-            <TextField
-                label="Enter a title"
-                variant={'outlined'}
-                className={error ? 'error' : ''}
-                value={title}
-                error={!!error}
-                helperText={error}
-                size={'small'}
-                onChange={changeItemHandler}
-                onKeyUp={addItemOnKeyUpHandler}
-            />
-            <IconButton onClick={addItemHandler} color={'primary'} disabled={disabled}>
-                <AddBoxIcon/>
-            </IconButton>
-        </div>
-    )
-})
+  return (
+    <div>
+      <TextField
+        label="Enter a title"
+        variant={"outlined"}
+        className={error ? "error" : ""}
+        value={title}
+        error={!!error}
+        helperText={error}
+        size={"small"}
+        onChange={changeItemHandler}
+        onKeyUp={addItemOnKeyUpHandler}
+      />
+      <IconButton onClick={addItemHandler} color={"primary"} disabled={disabled}>
+        <AddBoxIcon />
+      </IconButton>
+    </div>
+  );
+});
