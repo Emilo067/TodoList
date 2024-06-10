c395db5a717b6d5cb95c77823e12142eeb3e334f
Emilo067 <gasanovemil467@gmail.com>
2024-06-10 18:26:22 +0300

    Sprint 5

--- src/common/components/EditableSpan/EditableSpan.tsx
+++ src/common/components/EditableSpan/EditableSpan.tsx
@@ -1,40 +1,43 @@
-import {ChangeEvent, memo, useState} from "react";
+import { ChangeEvent, memo, useState } from "react";
 import TextField from "@mui/material/TextField/TextField";
 
 type PropsType = {
-    value: string
-    onChange: (value: string) => void
-    disabled: boolean
-}
+  value: string;
+  onChange: (value: string) => void;
+  disabled: boolean;
+};
 
-export const EditableSpan = memo(({disabled, value, onChange }: PropsType) => {
-    const [editMode, setEditMode] = useState(false)
-    const [title, setTitle] = useState(value)
+export const EditableSpan = memo(({ disabled, value, onChange }: PropsType) => {
+  const [editMode, setEditMode] = useState(false);
+  const [title, setTitle] = useState(value);
 
-    const editModeOn = () => {
-        setEditMode(true)
-    }
+  const editModeOn = () => {
+    setEditMode(true);
+  };
 
-    const editModeOff = () => {
-        setEditMode(false)
-        onChange(title)
-    }
+  const editModeOff = () => {
+    setEditMode(false);
+    onChange(title);
+  };
 
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setTitle(e.currentTarget.value)
-    }
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value);
+  };
 
-
-    return (
-        <>
-            {editMode && !disabled ? <TextField
-                variant={'outlined'}
-                value={title}
-                size={'small'}
-                onChange={onChangeHandler}
-                onBlur={editModeOff}
-                autoFocus
-            /> : <span onDoubleClick={editModeOn}>{title}</span>}
-        </>
-    )
-})
+  return (
+    <>
+      {editMode && !disabled ? (
+        <TextField
+          variant={"outlined"}
+          value={title}
+          size={"small"}
+          onChange={onChangeHandler}
+          onBlur={editModeOff}
+          autoFocus
+        />
+      ) : (
+        <span onDoubleClick={editModeOn}>{title}</span>
+      )}
+    </>
+  );
+});
