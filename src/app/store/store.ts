1fef06a32fc65d762f56a53195f5499ed613bd27
Emilo067 <gasanovemil467@gmail.com>
2024-07-22 18:49:17 +0300

    moved

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -3,12 +3,14 @@ import { ThunkDispatch } from "redux-thunk";
 import { configureStore } from "@reduxjs/toolkit";
 import { tasksReducer } from "features/TodolistList/model/tasks/tasks.reducer";
 import { todolistsReducer } from "features/TodolistList/model/todolists/todolists.reducer";
-import { appReducer } from "app/app.reducer";
+import { appReducer } from "app/model/app.reducer";
 import { authReducer } from "features/auth/model/auth.reducer";
+import { tasksPaginatorReducer } from "features/Paginator/model/tasksPaginator.reducer";
 
 export const store = configureStore({
   reducer: {
     tasks: tasksReducer,
+    tasksPaginator: tasksPaginatorReducer,
     todolists: todolistsReducer,
     app: appReducer,
     auth: authReducer,
