aa6fc44a9efe27722334b65831705986f1f5db8c
Emilo067 <gasanovemil467@gmail.com>
2024-07-22 18:51:05 +0300

    delete comments

--- src/features/Paginator/ui/Paginator.tsx
+++ src/features/Paginator/ui/Paginator.tsx
@@ -1,6 +1,5 @@
 import Pagination from "@mui/material/Pagination";
 import React from "react";
-import s from "./Paginator.module.css";
 import { useAppDispatch } from "common/hooks/useAppDispatch";
 import { tasksPaginatorThunks } from "features/Paginator/model/tasksPaginator.reducer";
 
@@ -12,13 +11,6 @@ type Props = {
 };
 
 export const Paginator = (props: Props) => {
-  // const totalUsersCount = useAppSelector<number>((state) => state.users.totalUsersCount);
-  // const pageSize = useSelector<number>((state) => state.users.pageSize);
-  // const currentPage = useAppSelector<number>((state) => state.users.currentPage);
-  //
-  // const initialPortionNumber = Math.ceil(currentPage / portionSize);
-  // const [portionNumber, setPortionNumber] = useState(initialPortionNumber);
-  // console.log(portionNumber);
   let pagesCount = Math.ceil((props.totalCount as number) / 10);
   const dispatch = useAppDispatch();
 
@@ -26,37 +18,7 @@ export const Paginator = (props: Props) => {
     dispatch(tasksPaginatorThunks.fetchTasksPage({ currentPage: page, todolistId: props.todolistId }));
   };
 
-  // const portionsCount = Math.ceil(pagesCount / portionSize);
-  // const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
-  // const rightPortionPageNumber = portionNumber * portionSize;
-  //
-  // const clickLeftPortionNumbePageHandler = () => {
-  //   setPortionNumber(portionNumber - 1);
-  // };
-  //
-  // const clickRightPortionNumbePageHandler = () => {
-  //   setPortionNumber(portionNumber + 1);
-  // };
-
   return (
-    // <S.PaginatorWrapperStyle>
-    //   {portionNumber > 1 && <button onClick={clickLeftPortionNumbePageHandler}>left</button>}
-    //   <S.PagesWrapper>
-    //     {pages
-    //       .filter((p) => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
-    //       .map((p) => (
-    //         <S.SelectedPage
-    //           onClick={() => {
-    //             onChangePage(p);
-    //           }}
-    //           isSelected={currentPage === p}
-    //         >
-    //           {p}
-    //         </S.SelectedPage>
-    //       ))}
-    //   </S.PagesWrapper>
-    //   {portionsCount > portionNumber && <button onClick={clickRightPortionNumbePageHandler}>right</button>}
-    // </S.PaginatorWrapperStyle>
     <div style={{ marginTop: "20px" }}>
       <Pagination
         onChange={(event, page) => {
